25efd1204c5839af76e50d44dafd8ae2236c9ee7
Joudsi <joudsayedissa@gmail.com>
2018-07-02 17:56:34 +0200

    fixed formating issues

--- page_spider.py
+++ page_spider.py
@@ -1,5 +1,5 @@
-import os
 import argparse
+
 
 def main(database: str, url_list_file: str):
     print("we are going to work with " + database)
@@ -8,9 +8,9 @@ def main(database: str, url_list_file: str):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-db", "--database", help = "SQLite file Name")
-    parser.add_argument("-i", "--input", help = "File containing urls to read")
+    parser.add_argument("-db", "--database", help="SQLite file Name")
+    parser.add_argument("-i", "--input", help="File containing urls to read")
     args = parser.parse_args()
     database_file = args.database
     input_file = args.input
-    main(database = database_file, url_list_file=input_file)
+    main(database=database_file, url_list_file=input_file)
