8187797ddfd9c40a05ebf65aac1bc70490f135e6
Joudsi <joudsayedissa@gmail.com>
2018-07-03 05:52:44 +0200

    added code to page_spider to call url_utilities and now the main function reads the url list successfully

--- utilities/url_utilities.py
+++ utilities/url_utilities.py
@@ -1,14 +1,13 @@
 import re
 import string
 from urllib.request import urlopen
-
 from bs4 import BeautifulSoup
 
 
 def load_urls_from_file(file_path: str):
     try:
         with open(file_path) as f:
-            content = f.readline()
+            content = f.readlines()
             return content
     except FileNotFoundError:
         print("the file " + file_path + " could not be found")
@@ -17,8 +16,8 @@ def load_urls_from_file(file_path: str):
 
 def load_page(url: str):
     response = urlopen(url)
-    html = response = response.read().decode('utf-8')
-    return
+    html = response.read().decode('utf-8')
+    return html
 
 
 def scrape_page(page_contents: str, false=None):
@@ -63,3 +62,4 @@ def scrape_page(page_contents: str, false=None):
                     print(".")
 
     return clean_words
+
